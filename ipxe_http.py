dba33c6bb303b34bcf71d52214e05d33618c1376
Michael Moese <mmoese@suse.de>
2019-03-21 14:25:37 +0100

    fix the import
    Signed-off-by: Michael Moese <mmoese@suse.de>

--- ipxe_http.py
+++ ipxe_http.py
@@ -68,5 +68,22 @@ class Ipxe_Http:
 
 
 if __name__ == "__main__":
-    server = Ipxe_Http(host='0.0.0.0', port='8080')
+    host = "0.0.0.0"
+    port = 8080
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-l", "--listen", help="hostname to listen on - defaults to all")
+    parser.add_argument("-p", "--port", type=int, help="specify listening port - defaults to 8080")
+
+    args = parser.parse_args()
+
+
+    if args.port:
+        port = args.port
+
+    if args.listen:
+        host - args.listen
+
+    server = Ipxe_Http(host=host, port=port)
     server.start()
